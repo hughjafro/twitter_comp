b55dc47caabc7431874ceee1d7568c69b6d0d26a
Christopher Tolbert <cmtolbert1@gmail.com>
2019-03-06 01:03:20 -0800

    Updated the app and model files with user/tweets

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,12 +1,14 @@
-"""Main application and routing logic for Twitter Comp """
+"""Main application and routing logic for Twitter Comp"""
 from flask import Flask
 from .models import DB
 
 
 def create_app():
     """Create and configure an instance of the Flask application."""
+    """config is basically a python dict"""
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     DB.init_app(app)
 
     @app.route('/')
