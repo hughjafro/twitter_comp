7399d132d4c4c71ed756596c8780dd0f66105fff
Christopher Tolbert <cmtolbert1@gmail.com>
2019-03-06 02:21:57 -0800

    added users and html file

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,6 +1,6 @@
 """Main application and routing logic for Twitter Comp"""
-from flask import Flask
-from .models import DB
+from flask import Flask, render_template, request
+from .models import DB, User
 
 
 def create_app():
@@ -9,10 +9,18 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['ENV'] = 'debug'
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        return 'Welcome to TwitOff!'
+        users = User.query.all()
+        return render_template('base.html', title='Home', users=users)
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='DB reset', users=[])
 
     return app
